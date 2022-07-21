63c53f4009ae522d83bcc83646f5a0ab6998b39e
BazarnovSergey <ssergey101@yandex.ru>
2022-07-21 20:55:10 +0300

    8. Join Point

--- src/main/java/aop/Test1.java
+++ src/main/java/aop/Test1.java
@@ -7,10 +7,11 @@ public class Test1 {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext(MyConfig.class);
-//        Book book = context.getBean("book", Book.class);
+        Book book = context.getBean("book", Book.class);
         UniLibrary uniLibrary = context.getBean("uniLibrary", UniLibrary.class);
         uniLibrary.getBook();
-        uniLibrary.getMagazine();
+        uniLibrary.addBook("Zaur", book);
+        uniLibrary.addMagazine();
 //        uniLibrary.addBook();
 //        uniLibrary.returnBook();
 //        uniLibrary.getMagazine();
--- src/main/java/aop/aspects/SecurityAspect.java
+++ src/main/java/aop/aspects/SecurityAspect.java
@@ -10,10 +10,11 @@ import org.springframework.stereotype.Component;
 @Order(20)
 public class SecurityAspect {
 
-    @Before("aop.aspects.MyPointcuts.allGetMethods()")
-    public void beforeGetSecurityAdvice() {
+    @Before("aop.aspects.MyPointcuts.allAddMethods()")
+    public void beforeAddSecurityAdvice() {
         System.out.println("beforeGetSecurityAdvice проверка прав на получение "
                 + "книги/журнала");
+        System.out.println("------------------------------------------");
     }
 
 }
